cb98daff8e321ce5e6515c775e1c608ef5d2148d
wuyuhang <wuyuhang@zhphfinance.com>
2019-01-24 18:14:18 +0800

    打包

--- config/index.js
+++ config/index.js
@@ -40,7 +40,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: '/vote-mobile/',
         /**
          * Source Maps
          */
